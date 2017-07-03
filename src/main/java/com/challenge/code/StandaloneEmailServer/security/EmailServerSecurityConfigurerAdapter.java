b9dbcf9cab168e1c23fd85619d18a402492c5256
seko <aelsayed.mo@gmail.com>
2017-07-03 18:39:34 +0200

    Fix REST authorization

--- src/main/java/com/challenge/code/StandaloneEmailServer/security/EmailServerSecurityConfigurerAdapter.java
+++ src/main/java/com/challenge/code/StandaloneEmailServer/security/EmailServerSecurityConfigurerAdapter.java
@@ -1,6 +1,5 @@
 package com.challenge.code.StandaloneEmailServer.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * Configuration adapter implementation that governs the authentication method. 
+ * Configuration adapter implementation that governs the authentication method.
  * 
  * Also defines which Rest requests to be authenticated.
  * 
@@ -19,24 +18,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class EmailServerSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private EmailServerBasicAuthenticationEntryPoint authenticationEntryPoint;
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// allow in memory authentication for mock user
-		auth.inMemoryAuthentication().withUser("mockUser").password("mockPassword").roles("MOCK_USER");
+		auth.inMemoryAuthentication().withUser("mockUser").password("mockPassword").roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// enable access to POST requests
-		http.authorizeRequests().antMatchers(HttpMethod.POST).permitAll().anyRequest().hasRole("MOCK_USER").and()
-				.httpBasic().authenticationEntryPoint(authenticationEntryPoint);
-		
-		// disable CSRF protection. 
-		// note: Of course it is recommended to protect against Cross Site Request Forgery but I am just show casing user authorization only because of time limit.
-		// Room for improvement is to implement a random token validation mechanism to verify incoming requests.
+		// authorize access to POST requests
+		http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.POST, "/SendEmail").hasRole("USER");
+
+		// disable CSRF protection.
+		// note: Of course it is recommended to protect against Cross Site Request
+		// Forgery but I am just show casing user authorization only because of time
+		// limit.
+		// Room for improvement is to implement a random token validation mechanism to
+		// verify incoming requests.
 		http.csrf().disable();
 	}
 
